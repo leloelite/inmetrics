e1c215541084ebe9df352e2ceb80474b9bf915c3
joseribeiro@tecgraf.puc-rio.br <joseribeiro@tecgraf.puc-rio.br>
2020-12-27 23:15:23 -0300

    Ajuste para hailitar chrome via jenkins

--- teste-inmetrics/src/test/java/br/com/inmetrics/teste/pageobjectscontrollers/Driver.java
+++ teste-inmetrics/src/test/java/br/com/inmetrics/teste/pageobjectscontrollers/Driver.java
@@ -3,12 +3,16 @@ package br.com.inmetrics.teste.pageobjectscontrollers;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
 	
 	WebDriver webDriver;
 	
 	public WebDriver instaciarNovaJanela() {
+		ChromeOptions options = new ChromeOptions();
+        String[] arguments = { "--disable-web-security", "--user-data-dir", "--allow-running-insecure-content", "--no-sandbox","--disable-dev-shm-usage" };
+        options.addArguments(arguments);
         System.setProperty("webdriver.chrome.driver","C:\\Users\\joseribeiro\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
 		webDriver = new ChromeDriver();
 		return webDriver;
