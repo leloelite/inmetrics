7f0a48ec66897a4e149f8590e154f71ce137cfb8
joseribeiro@tecgraf.puc-rio.br <joseribeiro@tecgraf.puc-rio.br>
2020-12-27 23:00:55 -0300

    Corrigindo path dos arquivos de steps

--- teste-inmetrics/src/test/java/br/com/inmetrics/teste/runner/RunCucumberTest.java
+++ teste-inmetrics/src/test/java/br/com/inmetrics/teste/runner/RunCucumberTest.java
@@ -9,7 +9,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 features = "src\\test\\java\\br\\com\\inmetrics\\teste\\features"
-,glue= {"glue"})
+,glue= "br.com.inmetrics.teste.glue"
+)
 public class RunCucumberTest {
 	 
 }
